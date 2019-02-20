cbe34318de0849e28ddd94a19eeeb4bf99b5c423
Russ Strickland <stricklandrw@gmail.com>
2019-02-19 21:41:31 -0500

    API Project models database definitions

--- models.py
+++ models.py
@@ -18,38 +18,11 @@ class Category(Base):
         'name' : self.name
         }
 
-    def __init__(self, id, name):
-        self.id = id
+    def __init__(self, name):
         self.name = name
 
     def __repr__(self):
         return "<Category('%s', '%s')>" % (self.id, self.name)
-
-class Item(Base):
-    __tablename__ = 'item'
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    description = Column(String)
-    cat_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-        'cat_id' : self.cat_id,
-        'description' : self.description,
-        'id' : self.id,
-        'title' : self.title
-        }
-
-    def __init__(self, id, title, description, cat_id):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.cat_id = cat_id
-
-    def __repr__(self):
-        return "<Item('%s', '%s', '%s', '%s')>" % (self.id, self.title, self.description, self.cat_id)
 
 class User(Base):
     __tablename__ = 'user'
@@ -57,13 +30,14 @@ class User(Base):
     username = Column(String(32), index=True)
     picture = Column(String)
     email = Column(String)
-    password_hash = Column(String(64))
 
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
+    def __init__(self, username, picture, email):
+        self.username = username
+        self.picture = picture
+        self.email = email
 
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
+    def __repr__(self):
+        return "<User('%s', '%s', '%s', '%s')>" % (self.id, self.username, self.picture, self.email)
 
     def generate_auth_token(self, expiration=600):
     	s = Serializer(secret_key, expires_in = expiration)
@@ -82,6 +56,35 @@ class User(Base):
     		return None
     	user_id = data['id']
     	return user_id
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+        'cat_id' : self.cat_id,
+        'description' : self.description,
+        'id' : self.id,
+        'title' : self.title,
+        'user_id' : self.user_id
+        }
+
+    def __init__(self, title, description, cat_id, user_id):
+        self.title = title
+        self.description = description
+        self.cat_id = cat_id
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "<Item('%s', '%s', '%s', '%s', '%s')>" % (self.id, self.title, self.description, self.cat_id, self.user_id)
 
 
 engine = create_engine('sqlite:///catalog.db')
